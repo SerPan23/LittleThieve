5bfeb7f64710e7033df6c97b015b5a825dc04705
SerPan23 <igracom@yandex.ru>
2021-03-31 16:25:47 +0300

    init project

---
+++ Source/LittleThieve.h
@@ -0,0 +1,10 @@
+#ifndef LITTLETHIEVE_H_INCLUDED
+#define LITTLETHIEVE_H_INCLUDED
+
+#include <SDL.h>
+#include <iostream.h>
+using namespace std;
+
+
+
+#endif // LITTLETHIEVE_H_INCLUDED
---
+++ Source/main.cpp
@@ -0,0 +1,8 @@
+#include "LittleThieve.h"
+
+int main()
+{
+
+
+    return 0;
+}
