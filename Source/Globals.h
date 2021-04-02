e6cd719f0f1af316b63924db8ca66b8e980a21c7
SerPan23 <igracom@yandex.ru>
2021-04-02 22:49:32 +0300

    add vector2

---
+++ Source/Globals.h
@@ -0,0 +1,18 @@
+#ifndef GLOBALS_H_INCLUDED
+#define GLOBALS_H_INCLUDED
+
+struct Vector2 {
+	int x, y;
+	Vector2() :
+		x(0), y(0)
+	{}
+	Vector2(int x, int y) :
+		x(x), y(y)
+	{}
+	Vector2 zero() {
+		return Vector2(0, 0);
+	}
+};
+
+
+#endif // GLOBALS_H_INCLUDED
--- Source/LittleThieve.h
+++ Source/LittleThieve.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <vector>
 
+#include "Globals.h"
 
 using namespace std;
 
