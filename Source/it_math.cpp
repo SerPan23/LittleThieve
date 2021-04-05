ab20e250b8c324d8b76c6af23e98654be1de1664
trettier <vipklopa@mail.ru>
2021-04-05 12:17:45 +0300

    Update it_math.cpp

--- Source/it_math.cpp
+++ Source/it_math.cpp
@@ -1,14 +1,14 @@
 #include "it_math.h"
 int it_min(int a, int b)
 {
-    if(a > b)
+    if(a < b)
         return a;
     return b;
 }
 
 double it_min(double a, double b)
 {
-    if(a > b)
+    if(a < b)
         return a;
     return b;
 }
