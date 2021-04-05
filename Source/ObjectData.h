0182b92a17d78e2f9894c674654026176333d278
SerPan23 <igracom@yandex.ru>
2021-04-05 21:48:54 +0300

    rename

---
+++ Source/ObjectData.h
@@ -0,0 +1,12 @@
+#ifndef OBJECTDATA_H_INCLUDED
+#define OBJECTDATA_H_INCLUDED
+
+struct ObjectData{
+    vector <vector<SDL_Color> > Texture;
+    int height;
+    int width;
+    int x, y;
+};
+
+
+#endif // OBJECTDATA_H_INCLUDED
