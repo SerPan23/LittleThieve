e91c06e7cb03f48a11a768b370f6bf8be8bcd9ec
SerPan23 <igracom@yandex.ru>
2021-04-05 20:21:26 +0300

    Create GameObject.h

---
+++ Source/GameObject.h
@@ -0,0 +1,12 @@
+#ifndef GAMEOBJECT_H_INCLUDED
+#define GAMEOBJECT_H_INCLUDED
+
+
+struct GameObject{
+    vector <vector<SDL_Color> > Texture;
+    int height;
+    int width;
+};
+
+
+#endif // GAMEOBJECT_H_INCLUDED
