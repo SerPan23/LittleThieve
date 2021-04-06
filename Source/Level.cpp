5c3b330e5e17449618b35c53fc80894ec0ac2ea9
Ziroxxx <60288822+Ziroxxx@users.noreply.github.com>
2021-04-06 18:44:39 +0300

    up

--- Source/Level.cpp
+++ Source/Level.cpp
@@ -13,7 +13,8 @@ Level::~Level() {}
 
 void Level::loadMap(int LevelIndex, Graphics &graphics)
 {
-
+    if(LevelIndex == 1)
+        Build_lvl_1(graphics.getRenderer(), walls);
 }
 
 void Level::update(int elapsedTime, Player &player)
@@ -23,5 +24,6 @@ void Level::update(int elapsedTime, Player &player)
 
 void Level::draw(Graphics &graphics)
 {
-
+    for(int i = 0; i < walls.size(); i++)
+        walls[i].drawWall(graphics.getRenderer());
 }
