79a9f3a376964bdaa688f8c3811a99fdde48bce1
Sergei <igracom@yandex.ru>
2021-04-07 11:03:10 +0300

    fix

--- Source/Player.cpp
+++ Source/Player.cpp
@@ -1,5 +1,4 @@
 #include "Player.h"
-#include "Level.h"
 
 void Player::drawPlayer(SDL_Renderer *renderer)
 {
@@ -28,10 +27,10 @@ void Player::moveStop(){
     this-> _dy = 0.0f;
 }
 
-bool Player::checkCollosion(vector <Wall> &walls){
+/*bool Player::checkCollosion(vector <Wall> &walls){
 
 }
-
+*/
 void Player::movePlayer(SDL_Event event)
 {
         if (event.key.keysym.scancode == SDL_SCANCODE_UP || event.key.keysym.scancode == SDL_SCANCODE_W)
--- Source/Player.h
+++ Source/Player.h
@@ -4,7 +4,7 @@
 #include "LittleThieve.h"
 #include "TextureFuncs.h"
 #include "SpriteData.h"
-#include "Level.h"
+//#include "Level.h"
 
 class Player{
     public:
@@ -31,7 +31,7 @@ class Player{
         void moveTop();
         void moveDown();
         void moveStop();
-        bool checkCollosion(vector <Wall> &walls);
+        //bool checkCollosion(vector <Wall> &walls);
 
         void update(float elapsedTime);
         void drawPlayer(SDL_Renderer *renderer);
