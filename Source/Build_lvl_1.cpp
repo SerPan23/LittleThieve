0f3baab8357be444b3f31cd9258ee86d23de92cc
Ziroxxx <60288822+Ziroxxx@users.noreply.github.com>
2021-04-20 13:48:28 +0300

    door_start

--- Source/Build_lvl_1.cpp
+++ Source/Build_lvl_1.cpp
@@ -6,7 +6,7 @@
 
 void    Build_lvl_1(SDL_Renderer *renderer, Vector2 &playerSpawnPoint, vector <Wall> &walls, vector <Item> &items, Floor &floor, int &necessaryPoints, float &levelTime)
 {
-    levelTime = 10;
+    levelTime = 30;
     playerSpawnPoint.x = 1334;
     playerSpawnPoint.y = 495;
     necessaryPoints = 1;
@@ -18,7 +18,10 @@ void    Build_lvl_1(SDL_Renderer *renderer, Vector2 &playerSpawnPoint, vector <W
     Wall main_wall_1(1, 98, 50, 1298, 48, 48); // 1, 50, 50, 1394, 48, 48
     Wall main_wall_2(1, 50, 98, 48, 818, 48); // 1, 50, 50, 48, 914, 48
     Wall main_wall_3(1, 98, 914, 1298, 48, 48); // 1, 50, 914, 1394, 48, 48
-    Wall main_wall_4(1, 1394, 98, 48, 818, 48); // 1, 1394, 50, 48, 914, 48
+    Wall main_wall_4(1, 1394, 98, 48, 336, 48); // 1, 1394, 50, 48, 914, 48
+    Wall main_wall_5(1, 1394, 594, 48, 336, 48);
+    Wall main_wall_4_end(1, 1394, 434, 48, 48, 48);
+    Wall main_wall_5_end(1, 1394, 546, 48, 48, 48);
     Wall main_wallAngle1(1, 50, 50, 48, 48, 48);
     Wall main_wallAngle2(1, 1394, 50, 48, 48, 48);
     Wall main_wallAngle3(1, 50, 914, 48, 48, 48);
@@ -36,6 +39,9 @@ void    Build_lvl_1(SDL_Renderer *renderer, Vector2 &playerSpawnPoint, vector <W
     main_wall_2.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall(138).bmp");
     main_wall_3.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall(111).bmp");
     main_wall_4.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall(138).bmp");
+    main_wall_4_end.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall_4_end(115).png");
+    main_wall_5.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall(138).bmp");
+    main_wall_5_end.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wall_5_end(141).png");
     main_wallAngle1.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wallAngle(109).bmp");
     main_wallAngle2.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wallAngle(110).bmp");
     main_wallAngle3.spriteData = loadTexture("..\\..\\Source\\Sprites\\main_wallAngle(136).bmp");
@@ -53,6 +59,9 @@ void    Build_lvl_1(SDL_Renderer *renderer, Vector2 &playerSpawnPoint, vector <W
     walls.push_back(main_wall_2);
     walls.push_back(main_wall_3);
     walls.push_back(main_wall_4);
+    walls.push_back(main_wall_4_end);
+    walls.push_back(main_wall_5);
+    walls.push_back(main_wall_5_end);
     walls.push_back(main_wallAngle1);
     walls.push_back(main_wallAngle2);
     walls.push_back(main_wallAngle3);
