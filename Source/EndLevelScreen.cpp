0568450475569a95f084149a0909f1c550344a26
SerPan23 <igracom@yandex.ru>
2021-04-21 21:55:04 +0300

    Update EndLevelScreen.cpp

--- Source/EndLevelScreen.cpp
+++ Source/EndLevelScreen.cpp
@@ -35,25 +35,11 @@ void EndLevelScreen::draw(Graphics &graphics)
     graphics.clear(21, 21, 21);
     if(isWin)
     {
-            /*SDL_Color c;
-            c.r = 0;
-            c.g = 255;
-            c.b = 0;
-            c.a = 255;
-            string msg = "YOU WIN!";
-            alphabet.drawText(graphics.getRenderer(), msg, 665, 450, c);*/
             SpriteData winText = loadTexture("..\\..\\Source\\Sprites\\winText.png");
             drawTexture(winText.texture, graphics.getRenderer(), 615, 350);
     }
     else
     {
-            /*SDL_Color c;
-            c.r = 255;
-            c.g = 0;
-            c.b = 0;
-            c.a = 255;
-            string msg = "YOU LOSE!";
-            alphabet.drawText(graphics.getRenderer(), msg, 645, 450, c);*/
             SpriteData loseText = loadTexture("..\\..\\Source\\Sprites\\loseText.png");
             drawTexture(loseText.texture, graphics.getRenderer(), 600, 350);
     }
