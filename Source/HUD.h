6fc8fa6b76b7b750d2b60e6e3ccf757c7c7db03c
SerPan23 <igracom@yandex.ru>
2021-04-26 18:21:54 +0300

    extarct draw score and time in own funcs

--- Source/HUD.h
+++ Source/HUD.h
@@ -25,6 +25,8 @@ public:
 
     void update(Alphabet alphabet, int playerScore, int maxScore, int t);
     void draw(SDL_Renderer *renderer);
+    void drawTime(SDL_Renderer *renderer);
+    void drawScore(SDL_Renderer *renderer);
 };
 
 #endif // HUD_H_INCLUDED
